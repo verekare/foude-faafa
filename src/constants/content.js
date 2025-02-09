2c84ae55db9f65480d21143791c1c0874532b062
Anastasia Shadrina <snasfibz05@gmail.com>
2025-02-09 12:42:58 +0300

    added content

---
+++ src/constants/content.js
@@ -0,0 +1,146 @@
+export default {
+  sound: [
+    {
+      header: "for animations",
+      details: [
+        "Composing music and music production.",
+        "Music sound design and various SFX creation.",
+        "Sonic solutions and sound selections that fit right for visuals.",
+        "Working with dynamics, mood and character of animation.",
+      ],
+      isExpanded: false
+    },
+    {
+      header: "for commercials",
+      details: [
+        "Communication with director and producers, finding right music and audio solutions for client's needs.",
+        "Reference selection.",
+        "Composing and music production.",
+        "Sound design and SFX creation.",
+        "Voice-over editing and mixing.",
+        "Mix and master.",
+      ],
+      isExpanded: false
+    },
+    {
+      header: "field recording and music for short video",
+      details: [
+        "Exploring and recording city sounds in Saigon, Vietnam.",
+        "Selection and arrangement recordings into one music collage.",
+        "Mixing and mastering."
+      ],
+      isExpanded: false
+    },
+    {
+      header: "linear sound design for YouTube videos",
+      details: [
+        "SFX, Foley, Ambience, Acoustic curtains creation.",
+        "Using different approaches and techniques: working with sound and sample libraries, layering, synthesis, recording, sampling/resamplimg.",
+        "Voice-over editing and mixing.",
+        "Mixing & Mastering to fit youtube’s technical requirements.",
+        "Operating on short time."
+      ],
+      isExpanded: false
+    },
+    {
+      header: "reverse engineering",
+      details: [
+        "Sound analysis.",
+        "Recreating life-like sounds of campfire in the woods by means of synths, processing and layering.",
+      ],
+      isExpanded: false
+    },
+    {
+      header: "motion video sound design",
+      details: [
+        "Creating athmospheric drones and ambienses.",
+        "Underlining movement and motion of the video.",
+      ],
+      isExpanded: false
+    },
+    {
+      header: "music for mood video",
+      details: ["Creating mood and “atmosphere” by means of music and little help of foley/ambience."],
+      isExpanded: false
+    },
+  ],
+  podcast: [
+    {
+      header: "podcast studios “<a href='https://bogemapodcasts.ru/' target='_blank'>Богема</a>”, “<a href='https://brainstorm.fm/' target='_blank'>Шторм</a>” and “<a href='https://tehnikarechi.studio/' target='_blank'>Техника речи</a>”",
+      details: [
+        "Audio logos for the studios “Техника речи” and “Богема” (you can hear them in the beginning <a href='https://youtu.be/3y7d1ByiBPE?si=X_ikGahR_t1f11tK' target='_blank'>here</a> and <a href='https://podcasts.apple.com/rs/podcast/%D0%BF%D0%BE%D0%BC%D0%B8%D0%B4%D0%BE%D1%80%D0%BA%D0%B8-%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D1%8C-%D0%B8-%D0%B1%D0%B0%D0%BD%D1%8F-%D1%87%D1%82%D0%BE-%D0%BF%D0%BE%D0%BC%D0%BE%D0%B3%D0%B0%D0%B5%D1%82-%D0%BD%D0%B0%D0%BC-%D1%85%D0%BE%D1%80%D0%BE%D1%88%D0%BE/id1648271635?i=1000651430328' target='_blank'>here</a>)",
+        "Editing and mixing for podcasts “<a href='https://podcasts.apple.com/ru/podcast/%D0%BD%D0%B5-%D0%B2%D1%81%D0%B5-%D0%B8%D0%B7-%D0%B4%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%B0/id1714560059' target='_blank'>Не все из детства</a>”, “<a href='https://podcasts.apple.com/ru/podcast/%D0%BD%D0%B0-%D1%8D%D1%82%D0%BE%D0%BC-%D0%BD%D0%B5-%D0%B7%D0%B0%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D0%B5%D1%88%D1%8C/id1656254270' target='_blank'>На этом не заработаешь</a>”, “<a href='https://podcasts.apple.com/kz/podcast/%D0%BA%D0%B0%D0%BA-%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE-%D0%BF%D1%80%D0%BE%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D1%8C-custdev-%D0%BE%D0%B1-%D0%B8%D1%81%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%D1%85-%D0%B0%D1%83%D0%B4%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B8/id1484670719?i=1000639459923' target='_blank'>Богема и маркетинг</a>”, “<a href='https://podcasts.apple.com/ru/podcast/%D0%BD%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE%D0%B4%D0%BD%D0%B8%D0%B9-%D1%81%D0%BF%D0%B5%D1%88%D0%BB-%D0%B4%D0%B5%D0%BB%D0%B0%D0%B5%D0%BC-%D1%80%D0%B0%D1%81%D0%BA%D0%BB%D0%B0%D0%B4-%D1%82%D0%B0%D1%80%D0%BE-%D0%BD%D0%B0-2024-%D0%B3%D0%BE%D0%B4/id1692635719?i=1000640079091' target='_blank'>Скоро 30</a>”",
+        "Audio repair and processing for podcast “<a href='https://podcasts.apple.com/lv/podcast/%D0%B8-%D1%82%D0%B5%D0%B1%D1%8F-%D0%B7%D0%B0%D0%BC%D0%B5%D0%BD%D1%8F%D1%82/id1706529062' target='_blank'>ИИ тебя заменит</a>” (starting with ep.2)",
+        "Editing for individual episodes of the podcasts “<a href='https://podcasts.apple.com/kz/podcast/%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D0%BF%D0%BB%D0%B0%D0%BD/id1618660424' target='_blank'>Бизнес-план</a>”, “<a href='https://podcasts.apple.com/kz/podcast/%D0%B4%D0%B5%D0%BB%D0%BE-%D0%BF%D1%80%D0%B8%D0%B2%D1%8B%D1%87%D0%BA%D0%B8/id1546099964' target='_blank'>Дело привычки</a>” and “<a href='https://podcasts.apple.com/kz/podcast/3-%D0%B1%D0%BB%D0%BE%D0%B3%D0%B5%D1%80-%D0%BF%D1%81%D0%B8%D1%85%D0%BE%D0%BB%D0%BE%D0%B3-%D0%B8-%D0%BD%D0%B0%D1%81%D1%82%D0%B0%D0%B2%D0%BD%D0%B8%D0%BA-%D0%BA%D1%82%D0%BE-%D1%82%D0%B0%D0%BA%D0%BE%D0%B9-%D1%83%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C-%D0%BD%D0%B0%D1%88%D0%B5%D0%B3%D0%BE/id1705681572?i=1000631202921' target='_blank'>Учитель нашего времени</a>”",
+      ],
+      isExpanded: false,
+    },
+    {
+      header: "podcast “What a day”",
+      details: [
+        "Editing, mixing and mastering",
+        "Within 30 days first episode held P1 in the category Personal Journals (Georgia & Belarus), P2 in the category Society & Culture (Belarus), P3 in the same Category in Georgia.",
+        '<iframe style="border-radius:12px;margin-top:14px" class="media" allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" frameborder="0" height="450" style="width:100%;max-width:560px;overflow:hidden;border-radius:10px;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.podcasts.apple.com/kz/podcast/what-a-day/id1672344303"></iframe>',
+      ],
+      isExpanded: false,
+    },
+    {
+      header: "jingles and sonic branding",
+      details: [
+        "Creating sonic branding for podcast studios “Богема” and “Техника речи”: a short audio logo in the beginning of each episode by these studios.",
+        "Unreleased jingle and outro for “What a day” podcast.",
+        "Editing, mixing and mastering for unreleased podcast + adapting song “Melikeyou” by Twit One into jingle and outro.",
+        '<iframe class="media" style="width:100%;max-width:390px;margin-top:14px" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1664420415%3Fsecret_token%3Ds-QJmvkpdhT9l&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+      ],
+      isExpanded: false,
+    }
+  ],
+  master: [
+    {
+      header: '<a href="https://open.spotify.com/album/6QthsP7w1zvrvw3HjfIcpo?si=fCU9UqTKT3ipDys7t5JiHQ" target="_blank">99oleg – хип-хоп истеричка</a>',
+      details: [
+        "Composing and producing hip-hop music",
+        '<iframe style="border-radius:12px;margin-top:14px" src="https://open.spotify.com/embed/album/2f5OJ31OBzt6ZRtoMTiO5l?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
+      ],
+      isExpanded: true,
+    },
+    {
+      header: "<a href='https://open.spotify.com/album/6QthsP7w1zvrvw3HjfIcpo?si=fCU9UqTKT3ipDys7t5JiHQ' target='_blank'>аня крутеева – последний альбом</a>",
+      details: [
+        "Composing and producing an album for an indie artist-songwriter",
+        '<iframe style="border-radius:12px;margin-top:14px" src="https://open.spotify.com/embed/album/6QthsP7w1zvrvw3HjfIcpo?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+      ],
+      isExpanded: true,
+    },
+    {
+      header: "selection of the other works",
+      details: ['<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1663034623%3Fsecret_token%3Ds-Jqzj104Vais&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>'],
+      isExpanded: true,
+    }
+  ],
+  about: [
+    {
+      header: "Live music",
+      details: [
+        "Drummer with an experience in rock music and passion for modern jazz.",
+        "Being a former member of several indie projects, I have a wide range of experience in studio recordings, live performances and tours.",
+        "First studio recorded album with my former group “<a href='https://open.spotify.com/artist/4BUju0fCIIQ08a01O2pIXS?si=gvKPK5MZTYeFsLM0JyeCXQ' target='_blank'>Sonya</a>” won “<a href='https://open.spotify.com/playlist/7H5mLiUsjtnSL8rP3bDwfY?si=VaOOnZBoTyeOwPvKEGnkCw' target='_blank'>Best emo” (2021)</a> nomination held by Russian independent music media and label “<a href='https://open.spotify.com/user/aplgr9vfve4ww4v41wspfsc3e?si=_i36ELtGStqvFKHpt_eVjQ' target='_blank'>Rodnoi Zvuk</a>”.",
+      ],
+      isExpanded: false,
+    },
+    {
+      header: "Education",
+      details: [
+        "BA Philosophy at Higher School of Economics (Moscow)",
+        "MA Philosophy and History of Religion at Higher School of Economics (Moscow) - <i>unfinished due to leaving Russia</i>",
+        "Music Production at Moscow Music School (under supervision of <a href='https://i-m-i.ru/skilled/record/andrey-ryzhkov' target='_blank'>Andrey Ryzhkov</a>) - <i>unfinished due to leaving Russia</i>",
+      ],
+      isExpanded: false,
+    },
+    {
+      header: "Contacts",
+      details: ["<a href='mailto:gibertandco@gmail.com'>e-mail</a>"],
+      isExpanded: false,
+    },
+  ],
+}
